432bac72e6029333daa66a875ee54a59556e6385
Matheus Castanho <mscastanho@gmail.com>
2016-09-13 21:38:40 -0300

    Almost done. Only drag functionality left to be done.

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
-all: compila executa clean
+all: compila clean executa
 
 compila:
 		g++ -c tinyxml2.cpp
-		g++ trab1.cpp -o trab1 tinyxml2.o -lGL -lGLU -lglut
-
-executa: 
-		./trab1 config.xml
+		g++ trab1.cpp -o trabalhocg tinyxml2.o -lGL -lGLU -lglut
 
 clean: 
 		@rm -f *.o
+
+executa: 
+		./trabalhocg ~/Documents/CG/config.xml
+
--- trab1.cpp
+++ trab1.cpp
@@ -11,9 +11,75 @@ using namespace std;
 GLfloat gx=0,gy=0;
 int keyStatus[256];
 
+int larguraJanela, alturaJanela;
+
+int tamQuadrado_2,xcQuadrado,ycQuadrado;
+
+GLfloat rQuadrado,gQuadrado,bQuadrado;
+
+bool existeQuadrado = false;
+
+// Guarda se o botao direito esta pressionado ou nao
+bool botaoEsqPressionado;
+int xInicialBotaoEsq;
+int yInicialBotaoEsq;
+
 void mouse(int botao, int estado, int x, int y)
 {
-	printf("X: %.2f ; Y: %.2f \n",x/250.0,(250-y)/250.0);
+	int metade = tamQuadrado_2;
+	
+	// Mudando a orientacao do eixo y
+	y = alturaJanela - y;
+	
+	if(botao == GLUT_LEFT_BUTTON && estado == GLUT_DOWN){
+		
+		if( (abs(x - xcQuadrado) <= tamQuadrado_2) && (abs(y - ycQuadrado) <= tamQuadrado_2) ){
+			botaoEsqPressionado = true;
+			xInicialBotaoEsq = x;
+			yInicialBotaoEsq = y;
+		}
+	
+	}
+	else{
+		botaoEsqPressionado = false;
+		xInicialBotaoEsq = -1;
+		yInicialBotaoEsq = -1;
+	}
+	
+	if(botao == GLUT_RIGHT_BUTTON && estado == GLUT_DOWN){
+		// O botao direito do mouse foi clicado
+		printf("xM: %d (%d,%d) \ny: %d (%d,%d)\n",x,xcQuadrado-metade,xcQuadrado+metade,y,ycQuadrado+metade,ycQuadrado-metade);
+		
+		if(existeQuadrado){
+			
+			// Checar se o clique foi dentro do quadrado
+			if( (abs(x - xcQuadrado) <= tamQuadrado_2) && (abs(y - ycQuadrado) <= tamQuadrado_2) ){
+				existeQuadrado = false;
+				glutPostRedisplay();
+			}
+			
+		}else{
+			// Nao existe quadrado ainda
+			xcQuadrado = x;
+			ycQuadrado = y;
+			existeQuadrado = true;
+			glutPostRedisplay();
+		}
+	}
+	
+}
+
+void motion(int x, int y){
+	
+	// Mudando a orientacao do eixo y
+	y = alturaJanela - y;
+	
+	if(botaoEsqPressionado){
+		xcQuadrado = xcQuadrado + (x - xInicialBotaoEsq);
+		ycQuadrado = ycQuadrado + (y - yInicialBotaoEsq);
+		glutPostRedisplay();
+	}
+	
 }
 
 void keyUp(unsigned char key, int x, int y)
@@ -55,22 +121,20 @@ void display(void)
 	glClear(GL_COLOR_BUFFER_BIT);
 	
 	/* Desenhar um polígono branco (retângulo) */
-	glColor3f(1.0,1.0,0);
-	glBegin(GL_POLYGON);
-		//glVertex3f(0.25+gx,0.25+gy,0.0);
-		//glVertex3f(0.75+gx,0.25+gy,0.0);
-		//glVertex3f(0.75+gx,0.75+gy,0.0);
-		//glVertex3f(0.25+gx,0.75+gy,0.0);
-		int i = 0;
-			for(i = 0 ; i < 5 ; i++ ){
-				glVertex3f(0.5*cos((90+i*72)*3.14/180.0)+gx,0.5*sin((90+i*72)*3.14/180.0)+gy,0.0);
-				glVertex3f(0.3*cos((126+i*72)*3.14/180.0)+gx,0.3*sin((126+i*72)*3.14/180.0)+gy,0.0);
-			}
-		//glVertex3f(0.30901+gx,0.95105+gy,0.0);
-		//glVertex3f(0.75+gx,0.25+gy,0.0);
-		//glVertex3f(0.75+gx,0.75+gy,0.0);
-		//glVertex3f(0.25+gx,0.75+gy,0.0);
-	glEnd();
+	if(existeQuadrado){
+		
+		float xcNormalizado = xcQuadrado/(float) larguraJanela;
+		float ycNormalizado = ycQuadrado/(float) alturaJanela;
+		float tamQ_2_Normalizado = tamQuadrado_2/(float) larguraJanela;
+		
+		glColor3f(rQuadrado,gQuadrado,bQuadrado);
+		glBegin(GL_POLYGON);
+			glVertex3f(xcNormalizado+tamQ_2_Normalizado,ycNormalizado+tamQ_2_Normalizado,0.0);
+			glVertex3f(xcNormalizado-tamQ_2_Normalizado,ycNormalizado+tamQ_2_Normalizado,0.0);
+			glVertex3f(xcNormalizado-tamQ_2_Normalizado,ycNormalizado-tamQ_2_Normalizado,0.0);
+			glVertex3f(xcNormalizado+tamQ_2_Normalizado,ycNormalizado-tamQ_2_Normalizado,0.0);
+		glEnd();
+	}
 
 	/* Não esperar */
 	//glFlush();
@@ -94,12 +158,12 @@ void idle (void)
 	glutPostRedisplay();
 }
 
-void init (void)
+void init (int r, int g, int b)
 {
 	/*selecionar cor de fundo (preto) */
-	glClearColor(0.0,0.0,0.0,0.0);
+	glClearColor((GLfloat) r,(GLfloat) g,(GLfloat) b,0.0);
 
-	/* inicializar sistema de voz */
+	
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(0.0,1.0,0.0,1.0,-1.0,1.0);
@@ -111,67 +175,80 @@ int main (int argc, char** argv)
 	char* filePath = NULL;
 	XMLDocument configFile;
 	
-	if (argc > 1 ){
+	// Confere se o arquivo XML foi passado como parametro de entrada
+	if (argc > 1 )
 		filePath = argv[1];
-		cout << "Esse eh o nome do arquivo: " << filePath << endl;
+	else{
+		cout << "Por favor entre com o arquivo XML" << endl;
+		cout << "Ex: trabalhocg <caminho para config.xml>" << endl;
+		return 0;
 	}
 	
 	if(filePath != NULL){
 		configFile.LoadFile(filePath);
 	}
 	
-	if(configFile.ErrorID() == 0){
-		XMLElement* janela = configFile.FirstChildElement("aplicacao")->FirstChildElement("janela");
-		XMLElement* quadrado = configFile.FirstChildElement("aplicacao")->FirstChildElement("quadrado");
-		XMLElement* fundo = janela->FirstChildElement("fundo");
-
-		const char* larguraJanela = janela->FirstChildElement("largura")->GetText();
-		const char* alturaJanela = janela->FirstChildElement("altura")->GetText();
-		const char* tituloJanela = janela->FirstChildElement("titulo")->GetText();
-		
-		printf("O titulo eh esse: %s\n O tamanho da janela eh: %sx%s\n",tituloJanela,larguraJanela,alturaJanela);
-		
-		int rFundo,gFundo,bFundo;
-		
-		fundo->QueryIntAttribute("corR",&rFundo);
-		fundo->QueryIntAttribute("corG",&gFundo);
-		fundo->QueryIntAttribute("corB",&bFundo);
-		
-		int tamQuad,rQuad,gQuad,bQuad;
-		
-		quadrado->QueryIntAttribute("tamanho",&tamQuad);
-		quadrado->QueryIntAttribute("corR",&rQuad);
-		quadrado->QueryIntAttribute("corG",&gQuad);
-		quadrado->QueryIntAttribute("corB",&bQuad);
-		
-		
-	
-		printf("Cor do fundo: \nR:%d \nG:%d \nB:%d\n\n",rFundo,gFundo,bFundo);
-		
-		printf("Stats do Quadrado: \nTamanho: %d\nR:%d \nG:%d \nB:%d\n",tamQuad,rQuad,gQuad,bQuad);
-		
-	}else
+	if(configFile.ErrorID() != 0)
 		cout << "Erro no arquivo de entrada config.xml" << endl;
-	
-	// zerar o vetor de status das teclas
-	memset(keyStatus,256,sizeof(int));
-	
-	/*glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
-	glutInitWindowSize(250,250);
-	glutInitWindowPosition(100,100);
-	glutCreateWindow("hello");
-	init();
-	glutDisplayFunc(display);
-	glutKeyboardFunc(keyPress);
-	glutKeyboardUpFunc(keyUp);
-	glutIdleFunc(idle);
-	glutMouseFunc(mouse);
+	else{
+		
+		XMLElement* janelaElement = configFile.FirstChildElement("aplicacao")->FirstChildElement("janela");
+		XMLElement* quadradoElement = configFile.FirstChildElement("aplicacao")->FirstChildElement("quadrado");
+		XMLElement* fundoElement = janelaElement->FirstChildElement("fundo");
 
-	glutMainLoop();
-*/
+		// Lendo o tamanho da janela
+		
+		janelaElement->FirstChildElement("largura")->QueryIntText(&larguraJanela);
+		janelaElement->FirstChildElement("altura")->QueryIntText(&alturaJanela);
 	
-	/* C ANSI requer que main retorne um inteiro */
+		// Lendo o titulo da janela
+		const char* tituloJanela = janelaElement->FirstChildElement("titulo")->GetText();
+		
+		GLfloat rFundo,gFundo,bFundo;
+		
+		// Lendo cor do fundo
+		fundoElement->QueryFloatAttribute("corR",&rFundo);
+		fundoElement->QueryFloatAttribute("corG",&gFundo);
+		fundoElement->QueryFloatAttribute("corB",&bFundo);
+		
+		// Lendo atributos do quadrado
+		int lado;
+		quadradoElement->QueryIntAttribute("tamanho",&lado);
+		tamQuadrado_2 = lado/2;
+		quadradoElement->QueryFloatAttribute("corR",&rQuadrado);
+		quadradoElement->QueryFloatAttribute("corG",&gQuadrado);
+		quadradoElement->QueryFloatAttribute("corB",&bQuadrado);
+		
+		// zerar o vetor de status das teclas
+		memset(keyStatus,256,sizeof(int));
+		
+		glutInit(&argc,argv);
+		
+		// Utilizar Double Buffer e esquema de cores RGB
+		glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
+		
+		// Definindo o tamanho da janela a ser aberta
+		glutInitWindowSize(larguraJanela,alturaJanela);
+		
+		// Escolher posicao da janela na tela
+		glutInitWindowPosition(250,250);
+		
+		// Definindo o titulo da janela
+		glutCreateWindow(tituloJanela);
+		
+
+		init(rFundo,gFundo,bFundo);
+		
+		glutDisplayFunc(display);
+		glutKeyboardFunc(keyPress);
+		glutKeyboardUpFunc(keyUp);
+		glutIdleFunc(idle);
+		glutMouseFunc(mouse);
+		glutMotionFunc(motion);
+		glutMainLoop();
+		
+	}
+
 	return 0;
 }
 
